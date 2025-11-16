6d855d31685e8a6f58700b2c5a34e846a5dfb434
Shobhit Katiyar <Shobhitkatiyar639@gmail.com>
2025-11-16 22:31:39 +0530

    final changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,6 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
-import axios from 'axios';
-
-axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
 
