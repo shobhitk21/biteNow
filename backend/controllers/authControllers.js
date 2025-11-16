31d3154a04491680eb3d7f2c4bfbfa0461de1d21
Shobhit Katiyar <Shobhitkatiyar639@gmail.com>
2025-11-16 11:31:04 +0530

    backend changes

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -37,8 +37,8 @@ const signUp = async (req, res) => {
 
         const token = await genToken(user._id);
         res.cookie("token", token, {
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true
         })
@@ -73,8 +73,8 @@ const signIn = async (req, res) => {
 
         const token = await genToken(user._id);
         res.cookie("token", token, {
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true
         })
@@ -176,10 +176,10 @@ const googleAuth = async (req, res) => {
 
         const token = genToken(User._id)
         res.cookie("token", token, {
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            httpOnly: true,
+            httpOnly: true
         })
         return res.status(200).json(user);
 
