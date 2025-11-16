a9a1b497a0a9b25bc5ae681c2659369fec82f03f
Shobhit Katiyar <Shobhitkatiyar639@gmail.com>
2025-11-16 22:27:43 +0530

    final changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
+import axios from 'axios';
 
 axios.defaults.withCredentials = true;
 
